b9387fee84beae19da94990a81016a3b2dad0ab2
Prateek <prateeknv99@gmail.com>
2017-10-14 14:36:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prateek/ccd_project/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.prateek.ccd_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.fbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+
+
+    }
+
